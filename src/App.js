fad90791f10df058f62715a825dc444c87442c3c
Aarif-Saifi <34440722+Aarif-Saifi@users.noreply.github.com>
2021-01-19 11:07:19 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ImageSlider from './component/ImageSlider';
+import { SliderData } from './component/SliderData';
+function App() {
+  return (
+    <ImageSlider slides={SliderData} />
+  );
+}
+
+export default App;
---
+++ src/component/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+    {
+        image:'https://images.pexels.com/photos/3368816/pexels-photo-3368816.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        image:'https://images.pexels.com/photos/5282513/pexels-photo-5282513.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        image:'https://images.pexels.com/photos/5302348/pexels-photo-5302348.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        image:'https://images.pexels.com/photos/5871515/pexels-photo-5871515.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        image:'https://images.pexels.com/photos/33041/antelope-canyon-lower-canyon-arizona.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+]
